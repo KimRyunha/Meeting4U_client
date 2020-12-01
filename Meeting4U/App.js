6d2ca3b8026fb40324e63913428a27fe3b148618
Jeong <dbwjdwn9216@gmail.com>
2020-12-01 21:50:15 +0900

    second
    배경 개념, ui 조금 바꿔봄

--- Meeting4U/App.js
+++ Meeting4U/App.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {Component} from 'react'
+import {Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-const App = () => {
-	return (
-		<SafeAreaView style={styles.container}>
-			<Text>Hello World</Text>
-		</SafeAreaView>
-	);
-};
+//type Props : props의 type을 정의해주는 것. 자료형을 제한해줄 때 사용함.
+type Props = {};
+//export default : 특정 변수(함수, 클래스 등)를 외부에서 사용할 수 있도록 해줌.
+//App 이름의 클래스가 Component를 상속받음으로서 React Native 앱에서 사용할 수 있는
+//컴포넌트가 됨. => render() 호출 가능해짐.
+export default class App extends Component<Props> {
+	//render 안에 작성하는게 화면에 보여짐.
+	//render()와 return()은 최상위는 하나의 컴포넌트로 덮여있어야 함.
+	render() {
+		return (
+	    	<View style={styles.container}>
+		    	<Text style={styles.welcome}>어서오세요 리액트의 숲에</Text>
+		    	<Text style={styles.instructions}>Meeting4U | {instructions}</Text>
+		    </View>
+		);
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+	}
+}
+
+// ios는 hello ios, android는 hello android 출력함.
+const instructions = Platform.select({
+	ios: 'Hello 앱등이',
+	android: 'Hello 삼엽충'
 });
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#33475E",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff'
+  },
+  instructions: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#333',
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Meeting4U_client
 # 프로젝트 생성 및 빌드
 ## 프로젝트 생성
+0. App.js에서 코드 작성
 1. react-native init Meeting4U
 2. cd Meeting4U
 
@@ -13,3 +14,28 @@ Build in Xcode
 ### Android
 react-native run-android
 npm run android
+
+# 배경 개념
+## 프로젝트 구조
+참고(https://yuddomack.tistory.com/entry/2React-Native-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EA%B8%B0%EB%B3%B8-%EA%B5%AC%EC%A1%B0-%EB%94%94%EB%B2%84%EA%B9%85?category=754156)
+1. 프로젝트 실행시 등장하는 커맨드 창은 추가 빌드 없이 reload를 가능하게 하는 번들링 서버.
+2. react 프로젝트는 /index.js를 기점으로 시작하므로 앱 구조에 따라 수정하면 됨.
+3. react native에서 지원하지 않는 기능은 네이티브 모듈로 구현하거나 모듈을 받아서 사용한다. 각 코드에 대한 설정은 android, ios directory에 작성된다.
+
+## 기본 컴포넌트
+참고(https://yuddomack.tistory.com/entry/3React-Native-Appjs%EC%99%80-JSX?category=754156)
+1. View
+2. Text
+3. Image
+4. TextInput
+5. ScrollView => 권장 X
+6. StyleSheet
+
+## 그외 컴포넌트(상시 추가)
+1. Platform : android와 ios를 구분지을 때 사용.
+2. StyleSheet : 컴포넌트들의 색상, 정렬, 크기, 높이 등 스타일(디자인)을 담당.
+
+#UI 레이아웃
+## flex, width, height
+flex : 크기를 다른 view와의 상대적 비율로 설정
+width. height : 크기를 고정 수치 or 퍼센트로 설정
