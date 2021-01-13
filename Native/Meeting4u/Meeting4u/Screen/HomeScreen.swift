74f039afb666d0376d2bcfb292c51a9e8ee69215
Jeong <dbwjdwn9216@gmail.com>
2021-01-13 22:42:01 +0900

    20210113
    20210113

---
+++ Native/Meeting4u/Meeting4u/Screen/HomeScreen.swift
@@ -0,0 +1,9 @@
+//
+//  HomeScreen.swift
+//  Meeting4u
+//
+//  Created by 유정주 on 2021/01/13.
+//  Copyright © 2021 Jeong. All rights reserved.
+//
+
+import Foundation
--- Native/Meeting4u/Meeting4u/Screen/LoginScreen.swift
+++ Native/Meeting4u/Meeting4u/Screen/LoginScreen.swift
@@ -11,6 +11,14 @@ import SwiftUI
 struct LoginScreen: View {
     @State var id: String = ""
     @State var pw: String = ""
+    @State var pw_ck: String = ""
+    @State var name: String = ""
+    @State var homeAddress: String = ""
+    @State var email: String = ""
+    @State var phoneNumber: String = ""
+
+    @State private var isSignup: Bool = false
+    @State private var isSignuping: Bool = false
     
     var body: some View {
         NavigationView {
@@ -23,7 +31,6 @@ struct LoginScreen: View {
                     .padding(.leading, 20)
                 
                 Spacer()
-                    .frame(height:100)
                 
                 VStack {
                     HStack {
@@ -31,12 +38,13 @@ struct LoginScreen: View {
                             .foregroundColor(Color.black)
                             .fontWeight(.bold)
                         
-                        Spacer().frame(width: 30)
+                        Spacer()
                         
                         TextField("", text: $id)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 180)
                     } // HStack
-                        .padding(.horizontal, 50)
+                        .padding(.horizontal, 20)
                         .padding(.top, 30)
                         .background(Color.white)
                     
@@ -45,15 +53,94 @@ struct LoginScreen: View {
                             .foregroundColor(Color.black)
                             .fontWeight(.bold)
                         
-                        Spacer().frame(width: 17)
+                        Spacer()
                         
                         SecureField("", text: $pw)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
-
+                            .frame(width: 180)
                     } // HStack
-                        .padding(.horizontal,50)
-                        .padding(.bottom, 30)
+                        .padding(.horizontal,20)
+                        .padding(.bottom, self.isSignup ? 0 : 30)
                         .background(Color.white)
+                    
+                    if self.isSignup {
+                        HStack {
+                            Text("비밀번호 확인")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            SecureField("", text: $pw_ck)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .frame(width: 180)
+
+                        } // HStack
+                            .padding(.horizontal,20)
+                            .background(Color.white)
+                        
+                        HStack {
+                            Text("이름")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            TextField("", text: $name)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .frame(width: 180)
+
+                        } // HStack
+                            .padding(.horizontal,20)
+                            .background(Color.white)
+                        
+                        HStack {
+                            Text("이메일")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            TextField("", text: $email)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .keyboardType(.emailAddress)
+                                .frame(width: 180)
+
+                        } // HStack
+                            .padding(.horizontal,20)
+                            .background(Color.white)
+                        
+                        HStack {
+                            Text("전화번호")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            TextField("", text: $phoneNumber)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .keyboardType(.numberPad)
+                                .frame(width: 180)
+
+                        } // HStack
+                            .padding(.horizontal,20)
+                            .background(Color.white)
+                        
+                        HStack {
+                            Text("주소")
+                                .foregroundColor(Color.black)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            SecureField("", text: $homeAddress)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .frame(width: 180)
+                        } // HStack
+                            .padding(.horizontal,20)
+                            .padding(.bottom, 30)
+                            .background(Color.white)
+                    }
                 } //VStack
                     .background(Color.white)
                     .cornerRadius(20)
@@ -69,22 +156,26 @@ struct LoginScreen: View {
                             .background(Color("dark_color"))
                             .foregroundColor(Color.white)
                             .cornerRadius(10)
-                        
                     }
                     
-                    NavigationLink(destination: Text("회원가입")) {
-                        Text("회원가입")
-                            .frame(width: 80, height: 10, alignment: .center)
-                            .font(.system(size: 20))
-                            .padding(15)
-                            .background(Color.gray)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(10)
+                    Button(action: {
+                        withAnimation {
+                            self.isSignup.toggle()
+                        }
+                    }) {
+                        Text(self.isSignup ? "완료" : "회원가입")
+                        .frame(width: 80, height: 10, alignment: .center)
+                        .font(.system(size: 20))
+                        .padding(15)
+                        .background(self.isSignup ? Color.blue : Color.gray)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
                     }
                     
                     Spacer()
                 }
 
+                Spacer()
                 Spacer()
             } //Out VStack
             .background(Color("main_color"))
@@ -93,5 +184,11 @@ struct LoginScreen: View {
             
         } //NavigationView
             
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
     }
 }
